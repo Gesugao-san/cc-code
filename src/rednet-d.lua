d8293e75dbc76c6c81bdeae265c66ff5d0cef857
Gesugao-san <gesugao.san@gmail.com>
2025-05-22 02:40:56 +0300

    Update rednet-d.lua

--- src/rednet-d.lua
+++ src/rednet-d.lua
@@ -1,4 +1,4 @@
--- wget https://github.com/Gesugao-san/cc-code/raw/refs/heads/master/src/rednet-d.lua rednet-d.lua
+-- rm rednet-d.lua .. wget https://github.com/Gesugao-san/cc-code/raw/refs/heads/master/src/rednet-d.lua rednet-d.lua
 master_id = 100
 
 local modem = peripheral.find("modem") or error("No modem attached", 0)
@@ -8,7 +8,7 @@ end
 
 while true do
   id, message = rednet.receive()
-  if id != master_id then goto continue end
+  if id ~= master_id then goto continue end
   if message == "hello" then
     print("Hello world!")
   end
