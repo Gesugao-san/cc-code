b4e7a7d3996792073dd119bac8f6cc4bf04a0777
Gesugao-san <gesugao.san@gmail.com>
2025-05-22 02:32:55 +0300

    Update get_modem.lua

--- src/get_modem.lua
+++ src/get_modem.lua
@@ -1,6 +1,11 @@
 -- Find all wireless modems connected to this computer.
 -- https://tweaked.cc/module/peripheral.html#v:find
 
-local modems = { peripheral.find("modem", function(name, modem)
-    return modem.isWireless() -- Check this modem is wireless.
-end) }
+--local modems = { peripheral.find("modem", function(name, modem)
+--  return modem.isWireless() -- Check this modem is wireless.
+--end) }
+
+local modems = { peripheral.find("monitor") }
+for _, modem in pairs(modem) do
+  modem.isWireless()
+end
