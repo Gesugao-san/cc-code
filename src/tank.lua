1c204c8d84e39908ea20100ce03d9cacdcfaf6b7
Gesugao-san <gesugao.san@gmail.com>
2025-05-23 03:18:55 +0300

    Add tank.lua

---
+++ src/tank.lua
@@ -0,0 +1,4 @@
+local tank = peripheral.find("create_enchantment_industry:disenchanter")
+print(tank)
+--tank.pullItems("Liquid Experience", 1)
+tank.pullItems("create_enchantment_industry:disenchanter", 1)
