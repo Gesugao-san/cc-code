b17d2e163f2e6de3cb419f12f1df29e571132896
Gesugao-san <gesugao.san@gmail.com>
2025-05-27 01:29:26 +0300

    Update time-print.lua

--- src/time-print.lua
+++ src/time-print.lua
@@ -1,4 +1,4 @@
-link = peripheral.find("create:display_link") or error("no create:display_link")
+link = peripheral.find("Create_DisplayLink") or error("no create:display_link")
 while true do
   link.clear()
   link.write(tostring(os.time()))
