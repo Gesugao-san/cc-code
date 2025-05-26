c0bf95970673dda45567b5bb559be3bb691ca0da
Gesugao-san <gesugao.san@gmail.com>
2025-05-27 01:30:42 +0300

    Update time-print.lua

--- src/time-print.lua
+++ src/time-print.lua
@@ -1,6 +1,7 @@
 link = peripheral.find("Create_DisplayLink") or error("no create:display_link")
 while true do
   link.clear()
+  link.setCursorPos(1,1)
   link.write(tostring(os.time()))
   link.update()
   sleep(1)
