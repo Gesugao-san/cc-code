de5b27e916b58f521a0459e4a7e637743e04d7a5
Gesugao-san <gesugao.san@gmail.com>
2025-05-27 15:48:44 +0300

    Update turtle.lua

--- src/turtle.lua
+++ src/turtle.lua
@@ -1,6 +1,20 @@
 turtle.getFuelLevel()
 turtle.refuel()
 while true do
+  select(3)
+  turtle.transferTo(1)
+  for var=19,2,-1 do
+    select(var)
+    turtle.transferTo(2)
+  end
+  select(2)
+  for var=1,16 do
+    turtle.forward()
+    turtle.turnRight()
+    turtle.place()
+    turtle.turnLeft()
+  end
+  select(1)
   turtle.forward()
   turtle.turnRight()
   turtle.place()
